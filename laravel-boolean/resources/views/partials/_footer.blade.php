78d443e599b4f6ee2b825f498801bcf510b81082
nisol91 <nicolasolzi91@gmail.com>
2019-02-19 11:30:46 +0100

    struttura fatta, manca un po di contenuto

--- laravel-boolean/resources/views/partials/_footer.blade.php
+++ laravel-boolean/resources/views/partials/_footer.blade.php
@@ -11,10 +11,17 @@
                 <a href="{{ route('static_page.workwithus') }}"><h2>Lavora con noi!</h2></a>
             </div>
             <div class="ft_iii">
-
+                <a href=""><i class="fab fa-facebook-square"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href=""><i class="fab fa-whatsapp-square"></i></a>
             </div>
         </div>
         <div class="fot_bottom">
-
+            <div class="fb_sx">
+                <h2>Boolean** (my_new_job > my_old_job) // returns true</h2>
+            </div>
+            <div class="fb_dx">
+                <h2>Boolean SRLS - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967</h2>
+            </div>
         </div>
     </div>
